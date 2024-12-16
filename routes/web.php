a5142d74b0edadd19bc2d2bb9f1d6a8722d2cd89
AlejandroMartinez999 <alejandro.martinez@devitm.com>
2024-12-15 19:13:55 -0600

    clase de rutas

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,37 @@ Route::get('person',function(){
     return$person->name();
     return$person->age();
 });
+
+route::get('/about',function(){
+    return "<h1> about page </h1>";
+})->name('about');
+
+route::get('/contact',function(){
+    return "<h1> contact page </h1>";
+});
+
+Route::get('/contact/{id}',function($id){
+    return $id;
+})->name('edit-contact');
+
+route::get('home', function(){
+    return "<a href='".route('edit-contact',1)."'> About</a>";
+});
+
+// grupo de rutas
+route::group(['prefix'=>'costumer'],function(){
+
+
+
+route::get('/',function(){
+    return "<h1> customer List</h1>";
+});
+
+route::get('/create',function(){
+    return "<h1> customer create</h1>";
+});
+
+route::get('/show',function(){
+    return "<h1> customer show</h1>";
+});
+});
